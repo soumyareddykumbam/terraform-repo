98b6cf71f0e25e3010b0d1f58f994659f4047444
soumyareddykumbam <138958516+soumyareddykumbam@users.noreply.github.com>
2023-07-08 05:41:23 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,10 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-}
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
-module "modulerg" {
-  source  = "C:\\Users\\user\\Desktop\\virtualnetwork123"
-  resource_group_name= "terraform_rg"
-  resource_group_location = "australia east"
-  azurerm_managed_disk=  "terraform disk"
-managed_instance_location= "australia east"
+
+resource "azurerm_resource_group" {
+name = "pipelinerg"
+location = "east us"
 }
-  
