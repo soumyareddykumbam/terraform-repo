9eec5bd00093314d7883410a2f95be39908930b9
soumyareddykumbam <138958516+soumyareddykumbam@users.noreply.github.com>
2023-07-09 04:54:27 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,12 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-name = "pipelinerg"
-location = "east us"
-tags = {env = "prod"}
+name = var.rgname
+location = var.loc
+}
+resource "azurerm_public_ip" "example" {
+  name                = var.pipname
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  allocation_method   = var.allocationmethod
 }
